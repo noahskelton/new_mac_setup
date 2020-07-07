78a176a9bd7352079b814dd8ac6aa2bd5c96c411
calmoo <skelton.noah@gmail.com>
2020-07-07 11:47:09 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+set -euxo pipefail
+
+# Homebrew
+# --------
+
+# Pass in /dev/null so we do not have a prompt
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+# curl -O https://raw.githubusercontent.com/adamtheturtle/new-mac-setup/master/Brewfile
+brew tap homebrew/bundle
+brew bundle
+rm -rf Brewfile
+
+# virtualenv
+pip3 install virtualenv
+# Without this, you get asked the first time you make a virtualenv
+mkdir -p ~/.virtualenvs
+
+curl -O https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Blue%20Matrix.itermcolors
+open "Blue Matrix.itermcolors"
+rm -rf "Blue Matrix.itermcolors"
+
+npm install -g spaceship-prompt
+
+
+# Set up git
+
+git config --global push.default current
+git config --global user.name "Noah Skelton"
+git config --global user.email skelton.noah@gmail.com
